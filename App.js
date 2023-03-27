911b1f6bccc12d094fc9efdc3ceff117b6b3be0a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-03-27 15:12:53 -0500

    update

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/corona.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Reyes Luxury Cars</Text>
+     <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  name:{
+    fontWeight: 'bold',
+    fontSize: '30px',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 128,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  }
 });
